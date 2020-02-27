de26ec09de221d786cae7b445c8b858a1c9177d5
Tomasz Zet <tomasz.zbwk@gmail.com>
2020-02-27 21:00:29 +0100

    added error messages

--- src/components/ErrorMessage/styled.tsx
+++ src/components/ErrorMessage/styled.tsx
@@ -2,5 +2,8 @@ import styled from "styled-components";
 
 export const StyledSpan = styled.span`
   color: FireBrick;
+  font-family: Arial, sans-serif;
   font-size: 0.8em;
+  position: absolute;
+  right: 0;
 `;
--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -4,6 +4,7 @@ import { useForm, IFormState } from "../../commons/Hooks/useForm";
 import FlexWrapperRow from "./../UI/FlexWrapperRow";
 import LoginFormField from "./../LoginFormField";
 import validation from "./../../commons/Validation";
+import ErrorMessage from "./../ErrorMessage";
 import { eventHandler, keyValuePair } from "./../../commons/Types";
 import Loader from "../Loader";
 
@@ -21,7 +22,9 @@ const LoginForm = (props: {}) => {
     isFetching: false
   };
 
-  const { formState, updateValue, values, submitForm } = useForm(initialState);
+  const { formState, updateValue, values, submitForm, errors } = useForm(
+    initialState
+  );
   const [passwordVisible, setPasswordVisible] = useState(false);
 
   const handleSubmit: eventHandler<HTMLFormElement> = event => {
@@ -56,6 +59,11 @@ const LoginForm = (props: {}) => {
             handleChange={updateValue}
           />
           {formFieldData[name].component}
+          {errors
+            .filter(({ field }) => field === name)
+            .map(item => (
+              <ErrorMessage message={item.message} />
+            ))}
         </FlexWrapperRow>
       ))}
       <SubmitStyled type="submit" value="Login" />
